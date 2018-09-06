645a216448394197b66872313649463bf3ddc5df
XnU132 <1328799161@qq.com>
2018-09-06 09:12:57 +0800

    initial..

--- AccountPad/main.js
+++ AccountPad/main.js
@@ -1,6 +1,8 @@
-var versions = ["1.0.0.6"];
+var versions = ["1.0.0.5", "1.0.0.6"];
 function ShowVersions() {
+    document.write('<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" media="screen" href="main.css" /></head><body>');
     versions.forEach(element => {
-        document.writeln("<a class='item' href='../InstallationPackages/" + element + ".rar'>版本：" + element + "</a>")
+        document.writeln("<a class='item' style='display:block' href='../InstallationPackages/" + element + ".rar'>版本：" + element + "</a>")
     });
+    document.writeln('</body></html>');
 }
