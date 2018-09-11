cc383e2c51c11698da51d7a23f4271c5080ced8d
XnU132 <1328799161@qq.com>
2018-09-11 13:35:50 +0800

    fix

--- AccountPad/main.js
+++ AccountPad/main.js
@@ -3,7 +3,7 @@ var versions = ["AccountPad1.0.0.5", "AccountPad1.0.0.6","AccountPad1.0.0.7","Ac
 function ShowVersions() {
     document.write('<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" media="screen" href="main.css" /></head><body>');
     versions.forEach(element => {
-        document.writeln("<a class='item' style='display:block' href='../AccountPad/InstallationPackages/" + element + ".rar'>版本：" + element + "</a>")
+        document.writeln("<a class='item' style='display:block' href='../AccountPad/InstallationPackages/" + element + ".rar'>点击下载：" + element + "</a>")
     });
     document.writeln('</body></html>');
 }
