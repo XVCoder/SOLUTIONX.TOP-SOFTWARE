b8aaa07b36564932c92627d78cad7ee4f12a8aaa
XnU132 <1328799161@qq.com>
2018-09-21 14:21:10 +0800

    update

--- AccountPad/main.js
+++ AccountPad/main.js
@@ -1,5 +1,5 @@
 var versions = ["AccountPad1.0.0.5", "AccountPad1.0.0.6","AccountPad1.0.0.7","AccountPad1.0.0.8","AccountPad1.0.1.1","AccountPad1.0.1.2"
-,"AccountPad1.0.1.3","AccountPad1.0.1.5","AccountPad1.0.1.8","AccountPad1.0.2.2"];
+,"AccountPad1.0.1.3","AccountPad1.0.1.5","AccountPad1.0.1.8","AccountPad1.0.2.2","AccountPad1.0.2.5"];
 function ShowVersions() {
     document.write('<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" media="screen" href="main.css" /></head><body>');
     versions.forEach(element => {
